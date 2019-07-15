aaf2e405fc7b3d1876798cfa279d2a1b7f9ceb5c
Matt Moore <mattmoore@carbonhelix.com>
2019-07-15 15:40:06 -0500

    Start adding button.

---
+++ src/nativeMain/kotlin/tornadofx/Button.kt
@@ -0,0 +1,13 @@
+package tornadofx
+
+import kotlinx.cinterop.*
+import libui.*
+
+class Button(
+  var title: String? = null,
+  var uiButton: CPointer<uiControl>? = null
+)
+
+fun button(init: Button.() -> Unit): Button = Button().apply(init).apply {
+  uiButton = uiNewButton("libui говорит: click me!")
+}
